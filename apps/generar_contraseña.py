f306f21b7c10d1696051d2acd045d5424229606a
Luis Amos <luisamos7@gmail.com>
2025-11-19 17:05:08 -0500

    v.2.2.1 | 19/11/2025 17:00

--- apps/generar_contraseña.py
+++
@@ -1,8 +0,0 @@
-from werkzeug.security import generate_password_hash, check_password_hash
-
-hash = generate_password_hash("sayhuite", method="scrypt")
-print(hash)
-
-estado = check_password_hash(hash, "sayhuite")
-
-print(estado)
