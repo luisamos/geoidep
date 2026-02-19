999954ae621cdd6bbfb2776d53ea034875e064a7
Luis Amos <luisamos7@gmail.com>
2026-02-19 18:14:14 -0500

    v.2.2.4 | 19/02/2025 18:00

---
+++ sql/capas_geograficas_19022026.sql
@@ -0,0 +1,32 @@
+BEGIN;
+-- 1) Vaciar en orden (hijo -> padre)
+
+TRUNCATE TABLE ide.def_servicios_geograficos,
+               ide.def_capas_geograficas
+RESTART IDENTITY CASCADE;
+
+-- 2) Cargar capas (padre)
+--\copy ide.def_capas_geograficas (id, nombre, descripcion, tipo_capa, publicar_geoperu, fecha_registro, id_categoria, id_institucion, usuario_crea, fecha_crea, usuario_modifica, fecha_modifica) FROM 'C:\apps\python\flask\geoidep\sql\def_capas_geograficas.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', QUOTE '"', NULL 'NULL', ENCODING 'UTF8');
+
+-- 3) Cargar servicios (hijo)
+--\copy ide.def_servicios_geograficos (id, id_capa, id_tipo_servicio, direccion_web, nombre_capa, titulo_capa, estado, id_layer, usuario_crea, fecha_crea, usuario_modifica, fecha_modifica) FROM 'C:\apps\python\flask\geoidep\sql\def_servicios_geograficos.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', QUOTE '"', NULL 'NULL', ENCODING 'UTF8');
+
+-- 4) Ajustar secuencias (IMPORTANTE si tus PK usan serial/identity con sequence)
+SELECT setval(pg_get_serial_sequence('ide.def_capas_geograficas','id'),
+              (SELECT COALESCE(MAX(id),1) FROM ide.def_capas_geograficas), true);
+
+SELECT setval(pg_get_serial_sequence('ide.def_servicios_geograficos','id'),
+              (SELECT COALESCE(MAX(id),1) FROM ide.def_servicios_geograficos), true);
+
+COMMIT;
+
+-- Validación rápida
+SELECT 'capas_geograficas' tabla, COUNT(*) total FROM ide.def_capas_geograficas
+UNION ALL
+SELECT 'servicios_geograficos' tabla, COUNT(*) total FROM ide.def_servicios_geograficos;
+
+SELECT *, b.* 
+FROM ide.def_capas_geograficas a
+INNER JOIN ide.def_servicios_geograficos b
+ON a.id = b.id_capa 
+WHERE nombre ILIKE ('%Unidad Ofertada%');
